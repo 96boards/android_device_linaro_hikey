62887bebaefc68a82b3afbde1bfd5d6e12b571a7
Vishal Bhoj <vishal.bhoj@linaro.org>
2015-06-28 11:19:18 +0530

    Fix the download of uefi fastboot tool in boot image
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- boot_fat.mk
+++ boot_fat.mk
@@ -1,5 +1,8 @@
 
 REALTOP=$(realpath $(TOP))
+FASTBOOT_EFI_BUILD_NUMBER := `wget -q --no-check-certificate -O - https://ci.linaro.org/job/96boards-hikey-uefi/lastSuccessfulBuild/buildNumber`
+FASTBOOT_EFI_URL := "http://builds.96boards.org/snapshots/hikey/linaro/uefi/${FASTBOOT_EFI_BUILD_NUMBER}/AndroidFastbootApp.efi"
+
 boot_fatimage: bootimage all_dtbs
 	mkdir -p $(PRODUCT_OUT)/boot_fat/
 	dd if=/dev/zero of=$(PRODUCT_OUT)/boot_fat.img bs=512 count=98304
@@ -11,13 +14,13 @@ boot_fatimage: bootimage all_dtbs
 	sudo cp $(REALTOP)/device/linaro/hikey/cmdline $(PRODUCT_OUT)/boot_tmp/cmdline
 	sync
 	sudo umount -f $(PRODUCT_OUT)/boot_tmp
-	FASTBOOT_EFI_BUILD_NUMBER=`wget -q --no-check-certificate -O - https://ci.linaro.org/job/96boards-hikey-uefi/lastSuccessfulBuild/buildNumber`
-	FASTBOOT_EFI_URL="http://builds.96boards.org/snapshots/hikey/linaro/uefi/${FASTBOOT_EFI_BUILD_NUMBER}/AndroidFastbootApp.efi"
 	dd if=/dev/zero of=$(PRODUCT_OUT)/boot_fat.uefi.img bs=512 count=98304
 	sudo mkfs.fat -n "BOOT IMG" $(PRODUCT_OUT)/boot_fat.uefi.img
 	mkdir -p $(PRODUCT_OUT)/boot_tmp && sudo mount -o loop,rw,sync $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/boot_tmp
 	sudo cp $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/Image $(PRODUCT_OUT)/boot_tmp
 	sudo cp $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/hi6220-hikey.dtb $(PRODUCT_OUT)/boot_tmp/hi6220-hikey.dtb
+	echo ${FASTBOOT_EFI_URL}
+	sudo wget --progress=dot ${FASTBOOT_EFI_URL} -O $(PRODUCT_OUT)/boot_tmp/fastboot.efi
 	sudo cp $(PRODUCT_OUT)/ramdisk.img $(PRODUCT_OUT)/boot_tmp/
 	sync
 	sudo umount -f $(PRODUCT_OUT)/boot_tmp
