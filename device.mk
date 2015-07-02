d7d37fd673ab1e80e05d3150b1ef8fb0546d7982
Vishal Bhoj <vishal.bhoj@linaro.org>
2015-07-02 10:44:33 +0530

    Run adb as root
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- device.mk
+++ device.mk
@@ -64,7 +64,9 @@ PRODUCT_PACKAGES += iontest \
 # Set zygote config
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64_32
 PRODUCT_PROPERTY_OVERRIDES += \
-         debug.sf.no_hw_vsync=1
+         debug.sf.no_hw_vsync=1 \
+         ro.secure=0 \
+         ro.adb.secure=0
 
 PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
 
