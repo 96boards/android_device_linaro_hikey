6f4234155f04883866edfe3c36871f1ad43ae6f8
Vishal Bhoj <vishal.bhoj@linaro.org>
2015-07-27 12:33:34 +0530

    Ship dtb that is compatible with hisilicon bootloader
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- boot_fat.mk
+++ boot_fat.mk
@@ -3,6 +3,7 @@ REALTOP=$(realpath $(TOP))
 FASTBOOT_EFI_BUILD_NUMBER := `wget -q --no-check-certificate -O - https://ci.linaro.org/job/96boards-hikey-uefi/lastSuccessfulBuild/buildNumber`
 FASTBOOT_EFI_URL := "http://builds.96boards.org/snapshots/hikey/linaro/uefi/${FASTBOOT_EFI_BUILD_NUMBER}/AndroidFastbootApp.efi"
 GRUB_EFI_URL := "http://builds.96boards.org/snapshots/hikey/linaro/grub/latest"
+HISI_BOOT_COMPAT_DTB := "http://builds.96boards.org/releases/hikey/linaro/debian/15.06/hi6220-hikey.dtb"
 
 boot_fatimage: bootimage all_dtbs
 	mkdir -p $(PRODUCT_OUT)/boot_fat/
@@ -10,7 +11,7 @@ boot_fatimage: bootimage all_dtbs
 	sudo mkfs.fat -n "BOOT IMG" $(PRODUCT_OUT)/boot_fat.img
 	mkdir -p $(PRODUCT_OUT)/boot_tmp && sudo mount -o loop,rw,sync $(PRODUCT_OUT)/boot_fat.img $(PRODUCT_OUT)/boot_tmp
 	sudo cp $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/Image $(PRODUCT_OUT)/boot_tmp
-	sudo cp $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/hi6220-hikey.dtb $(PRODUCT_OUT)/boot_tmp/lcb.dtb
+	sudo wget  --progress=dot ${HISI_BOOT_COMPAT_DTB} -O $(PRODUCT_OUT)/boot_tmp/lcb.dtb
 	sudo cp $(PRODUCT_OUT)/ramdisk.img $(PRODUCT_OUT)/boot_tmp
 	sudo cp $(REALTOP)/device/linaro/hikey/cmdline $(PRODUCT_OUT)/boot_tmp/cmdline
 	sync
