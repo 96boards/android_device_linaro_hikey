75ce655bc30071696d0d2fd3d4c93f7ec8d66d31
Vishal Bhoj <vishal.bhoj@linaro.org>
2015-11-10 12:12:49 +0530

    remove creation of boot_fat.img and remove use of sudo command
    Based on original work from John Stultz.
    
    We have removed the support of old boot_fat.img.
    
    boot_fat.uefi.img previously needed sudo command to create
    and copy files. with this change we don't need sudo command.
    
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- boot_fat.mk
+++ boot_fat.mk
@@ -6,29 +6,18 @@ GRUB_EFI_URL := "http://builds.96boards.org/snapshots/hikey/linaro/grub/latest"
 HISI_BOOT_COMPAT_DTB := "http://builds.96boards.org/releases/hikey/linaro/debian/15.06/hi6220-hikey.dtb"
 
 boot_fatimage: bootimage all_dtbs
-	mkdir -p $(PRODUCT_OUT)/boot_fat/
-	dd if=/dev/zero of=$(PRODUCT_OUT)/boot_fat.img bs=512 count=98304
-	sudo mkfs.fat -n "BOOT IMG" $(PRODUCT_OUT)/boot_fat.img
-	mkdir -p $(PRODUCT_OUT)/boot_tmp && sudo mount -o loop,rw,sync $(PRODUCT_OUT)/boot_fat.img $(PRODUCT_OUT)/boot_tmp
-	sudo cp $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/Image $(PRODUCT_OUT)/boot_tmp
-	sudo wget  --progress=dot ${HISI_BOOT_COMPAT_DTB} -O $(PRODUCT_OUT)/boot_tmp/lcb.dtb
-	sudo cp $(PRODUCT_OUT)/ramdisk.img $(PRODUCT_OUT)/boot_tmp
-	sudo cp $(REALTOP)/device/linaro/hikey/cmdline $(PRODUCT_OUT)/boot_tmp/cmdline
-	sync
-	sudo umount -f $(PRODUCT_OUT)/boot_tmp
-	dd if=/dev/zero of=$(PRODUCT_OUT)/boot_fat.uefi.img bs=512 count=98304
-	sudo mkfs.fat -n "BOOT IMG" $(PRODUCT_OUT)/boot_fat.uefi.img
-	mkdir -p $(PRODUCT_OUT)/boot_tmp && sudo mount -o loop,rw,sync $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/boot_tmp
-	sudo cp $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/Image $(PRODUCT_OUT)/boot_tmp
-	sudo cp $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/hi6220-hikey.dtb $(PRODUCT_OUT)/boot_tmp/hi6220-hikey.dtb
+	mkdir -p $(PRODUCT_OUT)/grub/
 	echo ${FASTBOOT_EFI_URL}
-	sudo wget --progress=dot ${FASTBOOT_EFI_URL} -O $(PRODUCT_OUT)/boot_tmp/fastboot.efi
-	sudo cp $(PRODUCT_OUT)/ramdisk.img $(PRODUCT_OUT)/boot_tmp/
-	sudo mkdir -p $(PRODUCT_OUT)/boot_tmp/grub/
-	sudo wget --progress=dot ${GRUB_EFI_URL}/grubaa64.efi -O $(PRODUCT_OUT)/boot_tmp/grubaa64.efi
-	sudo cp device/linaro/hikey/grub.cfg  $(PRODUCT_OUT)/boot_tmp/grub/
-	sync
-	sudo umount -f $(PRODUCT_OUT)/boot_tmp
-
+	wget --progress=dot ${FASTBOOT_EFI_URL} -O $(PRODUCT_OUT)/fastboot.efi
+	wget --progress=dot ${GRUB_EFI_URL}/grubaa64.efi -O $(PRODUCT_OUT)/grubaa64.efi
+	cp device/linaro/hikey/grub.cfg  $(PRODUCT_OUT)/grub/
+	dd if=/dev/zero of=$(PRODUCT_OUT)/boot_fat.uefi.img bs=512 count=98304
+	mkfs.fat -n "BOOT IMG" $(PRODUCT_OUT)/boot_fat.uefi.img
+	mcopy -i  $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/Image ::Image
+	mcopy -i  $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/hi6220-hikey.dtb ::hi6220-hikey.dtb
+	mcopy -i $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/fastboot.efi ::fastboot.efi
+	mcopy -i $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/grubaa64.efi ::grubaa64.efi
+	mcopy -i $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/ramdisk.img ::ramdisk.img
+	mcopy -s -i $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/grub/ ::
 
 droidcore: boot_fatimage
