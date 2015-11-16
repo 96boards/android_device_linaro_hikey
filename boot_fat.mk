d037e0c53820a6c1a48f9158f5eb7f03ecb99972
Vishal Bhoj <vishal.bhoj@linaro.org>
2015-11-16 16:13:51 +0530

    boot_fat: update efi binaries location
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- boot_fat.mk
+++ boot_fat.mk
@@ -6,18 +6,18 @@ GRUB_EFI_URL := "http://builds.96boards.org/snapshots/hikey/linaro/grub/latest"
 HISI_BOOT_COMPAT_DTB := "http://builds.96boards.org/releases/hikey/linaro/debian/15.06/hi6220-hikey.dtb"
 
 boot_fatimage: bootimage all_dtbs
-	mkdir -p $(PRODUCT_OUT)/grub/
+	mkdir -p $(PRODUCT_OUT)/EFI/BOOT/
 	echo ${FASTBOOT_EFI_URL}
 	wget --progress=dot ${FASTBOOT_EFI_URL} -O $(PRODUCT_OUT)/fastboot.efi
 	wget --progress=dot ${GRUB_EFI_URL}/grubaa64.efi -O $(PRODUCT_OUT)/grubaa64.efi
-	cp device/linaro/hikey/grub.cfg  $(PRODUCT_OUT)/grub/
+	cp device/linaro/hikey/grub.cfg  $(PRODUCT_OUT)/EFI/BOOT/
+	cp $(PRODUCT_OUT)/grubaa64.efi  $(PRODUCT_OUT)/EFI/BOOT/
+	cp $(PRODUCT_OUT)/fastboot.efi  $(PRODUCT_OUT)/EFI/BOOT/
 	dd if=/dev/zero of=$(PRODUCT_OUT)/boot_fat.uefi.img bs=512 count=98304
 	mkfs.fat -n "BOOT IMG" $(PRODUCT_OUT)/boot_fat.uefi.img
 	mcopy -i  $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/Image ::Image
 	mcopy -i  $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/hi6220-hikey.dtb ::hi6220-hikey.dtb
-	mcopy -i $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/fastboot.efi ::fastboot.efi
-	mcopy -i $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/grubaa64.efi ::grubaa64.efi
 	mcopy -i $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/ramdisk.img ::ramdisk.img
-	mcopy -s -i $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/grub/ ::
+	mcopy -s -i $(PRODUCT_OUT)/boot_fat.uefi.img $(PRODUCT_OUT)/EFI/ ::
 
 droidcore: boot_fatimage
