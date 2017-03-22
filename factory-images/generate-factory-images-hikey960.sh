96boards/android_device_linaro_hikey

commit c2752f1b7c998b4cad1ddc8a3c7a4ce883c01b62

hikey: Fix up merge with AOSP/master
The last merge ended up going awry and what was committed was
the same tree state as before that merge
(9388425ad012 - hikey960: factory-images: Fixup dt.img path...).

So this commit fixes that up to what I believe it ought to be.

Change-Id: I4e49854d173d42d2df293ebcdbeefc0c0ca3e613
Signed-off-by: John Stultz <john.stultz@linaro.org>

factory-images/generate-factory-images-hikey960.sh
@@ -41,8 +41,8 @@ cp $ANDROID_BUILD_TOP/$DEVICE_DIR/installer/hikey960/README tmp/$PRODUCT-$VERSIO
 cp $ANDROID_BUILD_TOP/$DEVICE_DIR/installer/hikey960/sec_xloader.img tmp/$PRODUCT-$VERSION/
 cp $ANDROID_BUILD_TOP/$DEVICE_DIR/installer/hikey960/vector.img tmp/$PRODUCT-$VERSION/
 
-# copy over dt.img
-cp $ANDROID_BUILD_TOP/out/target/product/hikey960/dt.img tmp/$PRODUCT-$VERSION/
+# copy over dts.img
+cp $ANDROID_BUILD_TOP/$DEVICE_DIR/../hikey-kernel/dt-hikey960.img tmp/$PRODUCT-$VERSION/
 
 # copy over the update image
 cp ${SRCPREFIX}$PRODUCT-img-$BUILD.zip tmp/$PRODUCT-$VERSION/image-$PRODUCT-$VERSION.zip
@@ -80,7 +80,7 @@ fastboot flash nvme		nvme.img
 fastboot flash vector		vector.img
 fastboot flash fw_lpm3		lpm3.img
 fastboot flash trustfirmware	bl31.bin
-fastboot flash dts		dt.img
+fastboot flash dts		dt-hikey960.img
 
 # XXX fastboot update doesn't format cache and userdata
 # XXX so flash those manually. Remove this later.

Android.bp
@@ -1,0 +1,18 @@
+//
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+subdirs = [
+    "bluetooth",
+]

BoardConfigCommon.mk
@@ -20,7 +20,6 @@ USE_OPENGL_RENDERER := true
 ANDROID_ENABLE_RENDERSCRIPT := true
 
 # BT configs
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/linaro/hikey/bluetooth
 BOARD_HAVE_BLUETOOTH := true
 
 # generic wifi

bluetooth/Android.bp
@@ -1,0 +1,41 @@
+//
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+cc_binary {
+    name: "android.hardware.bluetooth@1.0-service.hikey",
+    proprietary: true,
+    relative_install_path: "hw",
+    srcs: [
+        "bluetooth_hci.cc",
+        "hci_packetizer_hikey.cc",
+        "service.cc",
+    ],
+    shared_libs: [
+        "android.hardware.bluetooth@1.0",
+        "libbase",
+        "libcutils",
+        "libhardware",
+        "libhwbinder",
+        "libhidlbase",
+        "libhidltransport",
+        "liblog",
+        "libutils",
+    ],
+    static_libs: [
+        "android.hardware.bluetooth-async",
+        "android.hardware.bluetooth-hci",
+    ],
+    init_rc: ["android.hardware.bluetooth@1.0-service.hikey.rc"],
+}

bluetooth/android.hardware.bluetooth@1.0-service.hikey.rc
@@ -1,0 +1,4 @@
+service bluetooth-1-0 /vendor/bin/hw/android.hardware.bluetooth@1.0-service.hikey
+    class hal
+    user bluetooth
+    group bluetooth

bluetooth/bdroid_buildcfg.h
@@ -1,23 +1,0 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef _BDROID_BUILDCFG_H
-#define _BDROID_BUILDCFG_H
-
-#define BTM_DEF_LOCAL_NAME   "Android Bluedroid"
-#define PRELOAD_START_TIMEOUT_MS 30000
-
-#endif

bluetooth/bluetooth_hci.cc
@@ -1,0 +1,152 @@
+//
+// Copyright 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#define LOG_TAG "android.hardware.bluetooth@1.0.hikey"
+
+#include "bluetooth_hci.h"
+
+#include <utils/Log.h>
+
+#include <android-base/logging.h>
+
+#include "hci_internals.h"
+
+namespace {
+
+using android::hardware::bluetooth::V1_0::hikey::BluetoothHci;
+using android::hardware::hidl_vec;
+
+BluetoothHci* g_bluetooth_hci = nullptr;
+
+size_t write_safely(int fd, const uint8_t* data, size_t length) {
+  size_t transmitted_length = 0;
+  while (length > 0) {
+    ssize_t ret =
+        TEMP_FAILURE_RETRY(write(fd, data + transmitted_length, length));
+
+    if (ret == -1) {
+      if (errno == EAGAIN) continue;
+      ALOGE("%s error writing to UART (%s)", __func__, strerror(errno));
+      break;
+
+    } else if (ret == 0) {
+      // Nothing written :(
+      ALOGE("%s zero bytes written - something went wrong...", __func__);
+      break;
+    }
+
+    transmitted_length += ret;
+    length -= ret;
+  }
+
+  return transmitted_length;
+}
+
+}  // namespace
+
+namespace android {
+namespace hardware {
+namespace bluetooth {
+namespace V1_0 {
+namespace hikey {
+
+Return<void> BluetoothHci::initialize(
+    const ::android::sp<IBluetoothHciCallbacks>& cb) {
+  ALOGI("BluetoothHci::initialize()");
+
+  CHECK(cb != nullptr);
+  event_cb_ = cb;
+
+  hci_tty_fd_ = open("/dev/hci_tty", O_RDWR);
+  if (hci_tty_fd_ < 0) {
+    ALOGE("%s: Can't open hci_tty", __func__);
+    event_cb_->initializationComplete(Status::INITIALIZATION_ERROR);
+  }
+
+  CHECK(g_bluetooth_hci == nullptr) << __func__ << " is not reentrant";
+  g_bluetooth_hci = this;
+
+  fd_watcher_.WatchFdForNonBlockingReads(
+      hci_tty_fd_, [this](int fd) { hci_packetizer_.OnDataReadyHikey(fd); });
+
+  event_cb_->initializationComplete(Status::SUCCESS);
+  return Void();
+}
+
+Return<void> BluetoothHci::close() {
+  ALOGW("BluetoothHci::close()");
+  ::close(hci_tty_fd_);
+  hci_tty_fd_ = -1;
+  g_bluetooth_hci = nullptr;
+  return Void();
+}
+
+Return<void> BluetoothHci::sendHciCommand(const hidl_vec<uint8_t>& packet) {
+  uint8_t type = HCI_PACKET_TYPE_COMMAND;
+  int rv = write_safely(hci_tty_fd_, &type, sizeof(type));
+  if (rv == sizeof(type))
+    rv = write_safely(hci_tty_fd_, packet.data(), packet.size());
+  return Void();
+}
+
+Return<void> BluetoothHci::sendAclData(const hidl_vec<uint8_t>& packet) {
+  uint8_t type = HCI_PACKET_TYPE_ACL_DATA;
+  int rv = write_safely(hci_tty_fd_, &type, sizeof(type));
+  if (rv == sizeof(type))
+    rv = write_safely(hci_tty_fd_, packet.data(), packet.size());
+  return Void();
+}
+
+Return<void> BluetoothHci::sendScoData(const hidl_vec<uint8_t>& packet) {
+  uint8_t type = HCI_PACKET_TYPE_SCO_DATA;
+  int rv = write_safely(hci_tty_fd_, &type, sizeof(type));
+  if (rv == sizeof(type))
+    rv = write_safely(hci_tty_fd_, packet.data(), packet.size());
+  return Void();
+}
+
+BluetoothHci* BluetoothHci::get() { return g_bluetooth_hci; }
+
+void BluetoothHci::OnPacketReady() {
+  BluetoothHci::get()->HandleIncomingPacket();
+}
+
+void BluetoothHci::HandleIncomingPacket() {
+  HciPacketType hci_packet_type = hci_packetizer_.GetPacketType();
+  hidl_vec<uint8_t> hci_packet = hci_packetizer_.GetPacket();
+
+  switch (hci_packet_type) {
+    case HCI_PACKET_TYPE_EVENT:
+      event_cb_->hciEventReceived(hci_packet);
+      break;
+    case HCI_PACKET_TYPE_ACL_DATA:
+      event_cb_->aclDataReceived(hci_packet);
+      break;
+    case HCI_PACKET_TYPE_SCO_DATA:
+      event_cb_->scoDataReceived(hci_packet);
+      break;
+    default: {
+      bool hci_packet_type_corrupted = true;
+      CHECK(hci_packet_type_corrupted == false);
+    }
+  }
+}
+
+}  // namespace hikey
+}  // namespace V1_0
+}  // namespace bluetooth
+}  // namespace hardware
+}  // namespace android

bluetooth/bluetooth_hci.h
@@ -1,0 +1,63 @@
+//
+// Copyright 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#pragma once
+
+#include <android/hardware/bluetooth/1.0/IBluetoothHci.h>
+
+#include <hidl/MQDescriptor.h>
+
+#include "async_fd_watcher.h"
+#include "hci_packetizer_hikey.h"
+
+namespace android {
+namespace hardware {
+namespace bluetooth {
+namespace V1_0 {
+namespace hikey {
+
+using ::android::hardware::Return;
+using ::android::hardware::hidl_vec;
+
+class BluetoothHci : public IBluetoothHci {
+ public:
+  Return<void> initialize(
+      const ::android::sp<IBluetoothHciCallbacks>& cb) override;
+  Return<void> sendHciCommand(const hidl_vec<uint8_t>& packet) override;
+  Return<void> sendAclData(const hidl_vec<uint8_t>& packet) override;
+  Return<void> sendScoData(const hidl_vec<uint8_t>& packet) override;
+  Return<void> close() override;
+
+  static void OnPacketReady();
+
+  static BluetoothHci* get();
+
+ private:
+  ::android::sp<IBluetoothHciCallbacks> event_cb_;
+  int hci_tty_fd_;
+
+  void HandleIncomingPacket();
+
+  async::AsyncFdWatcher fd_watcher_;
+
+  HciPacketizerHikey hci_packetizer_{BluetoothHci::OnPacketReady};
+};
+
+}  // namespace hikey
+}  // namespace V1_0
+}  // namespace bluetooth
+}  // namespace hardware
+}  // namespace android

commit 91dca030d6d9860c66f177c32402ba1398491e7d

fix dt copy error from manual merge
commit: c2752f1b7c998b4cad1dd broke dt flashing in
factory image. This should fix it.

Change-Id: I187e9741d67fa578c686751a97325ca2cd148008
Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

factory-images/generate-factory-images-hikey960.sh
@@ -42,7 +42,7 @@ cp $ANDROID_BUILD_TOP/$DEVICE_DIR/installer/hikey960/sec_xloader.img tmp/$PRODUC
 cp $ANDROID_BUILD_TOP/$DEVICE_DIR/installer/hikey960/vector.img tmp/$PRODUCT-$VERSION/
 
 # copy over dts.img
-cp $ANDROID_BUILD_TOP/$DEVICE_DIR/../hikey-kernel/dt-hikey960.img tmp/$PRODUCT-$VERSION/
+cp $ANDROID_BUILD_TOP/out/target/product/hikey960/dt.img tmp/$PRODUCT-$VERSION/
 
 # copy over the update image
 cp ${SRCPREFIX}$PRODUCT-img-$BUILD.zip tmp/$PRODUCT-$VERSION/image-$PRODUCT-$VERSION.zip
@@ -80,7 +80,7 @@ fastboot flash nvme		nvme.img
 fastboot flash vector		vector.img
 fastboot flash fw_lpm3		lpm3.img
 fastboot flash trustfirmware	bl31.bin
-fastboot flash dts		dt-hikey960.img
+fastboot flash dts		dt.img
 
 # XXX fastboot update doesn't format cache and userdata
 # XXX so flash those manually. Remove this later.